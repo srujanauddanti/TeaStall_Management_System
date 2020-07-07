72aeaaad8317d98343f1239431e5dea777ef528a
srujanauddanti <67875557+srujanauddanti@users.noreply.github.com>
2020-07-07 14:19:34 +0530

    Stall Management
    Stall Management

---
+++ TeaStall_Management_System/Controllers/InventoryController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeaStall_Management_System.Models;
+
+namespace TeaStall_Management_System.Controllers
+{
+    public class InventoryController : Controller
+    {
+        InventoryManagementEntities Obj = new InventoryManagementEntities();
+        string message = "";
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>  
+        ///   
+        /// Get All Inventory  
+        /// </summary>  
+        /// <returns></returns>  
+        public JsonResult Get_AllInventory()
+        {
+            using (InventoryManagementEntities Obj = new InventoryManagementEntities())
+            {
+                List<TeaInventory> item = Obj.TeaInventories.ToList();
+                return Json(item, JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Get Inventory With Id  
+        /// </summary>  
+        /// <param name="Id"></param>  
+        /// <returns></returns>  
+        public ActionResult Get_InventoryById(int id)
+        {
+            using (InventoryManagementEntities Obj = new InventoryManagementEntities())
+            {
+                TeaInventory item = Obj.TeaInventories.Find(id);
+                return View(item);
+               // return Json(Obj.TeaInventories.Find(id), JsonRequestBehavior.AllowGet);
+            }
+        }
+        /// <summary>  
+        /// Insert New Item  
+        /// </summary>  
+        /// <param name="item"></param>  
+        /// <returns></returns>  
+        public JsonResult Insert_Inventory(TeaInventory item)
+        {
+            if (item!= null)
+            {
+                using (InventoryManagementEntities Obj = new InventoryManagementEntities())
+                {
+                    Obj.TeaInventories.Add(item);
+                    if(Obj!=null)
+                    {
+                        Obj.SaveChanges();
+                        message= "Item Added Successfully";
+                    }
+                    else
+                    {
+                        message= "Item Not Inserted! Try Again";
+                    }
+
+
+                }
+            }
+            else
+            {
+                message= "Item Not Inserted! Try Again";
+            }
+            return Json(message, JsonRequestBehavior.AllowGet);
+
+        }
+        /// <summary>  
+        /// Delete Item Information  
+        /// </summary>  
+        /// <param name="item"></param>  
+        /// <returns></returns>  
+        public JsonResult Delete_Inventory(TeaInventory item)
+        {
+            if (item != null)
+            {
+                using (InventoryManagementEntities Obj = new InventoryManagementEntities())
+                {
+                    var item_ = Obj.Entry(item);
+                    if (item_.State == System.Data.Entity.EntityState.Detached)
+                    {
+                        Obj.TeaInventories.Attach(item);
+                        Obj.TeaInventories.Remove(item);
+                    }
+                    Obj.SaveChanges();
+                    message= "Item Deleted Successfully";
+                }
+            }
+            else
+            {
+                message= "Item Not Deleted! Try Again";
+            }
+            return Json(message, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>  
+        /// Update Item Information  
+        /// </summary>  
+        /// <param name="item"></param>  
+        /// <returns></returns>  
+        public JsonResult Update_Inventory(TeaInventory item)
+        {
+            
+            if (item != null&&item.Sr_no>0)
+            {
+                    var item_ = Obj.Entry(item);
+                TeaInventory itemObj = Obj.TeaInventories.Where(x => x.Sr_no == item.Sr_no).FirstOrDefault();
+                if(itemObj!=null)
+                {
+                    itemObj.TeaName = item.TeaName;
+                    itemObj.Description = item.Description;
+                    itemObj.Price = item.Price;
+                    Obj.TeaInventories.Attach(itemObj);
+                    Obj.Entry(itemObj).State = EntityState.Modified;
+                    Obj.SaveChanges();
+                    
+                }
+
+                message= "Inventory Updated Successfully";
+
+            }
+            else
+            {
+                message= "Inventory Not Updated! Try Again";
+            }
+            return Json(message, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ TeaStall_Management_System/scripts/AngularController.js
@@ -0,0 +1,135 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    debugger;
+    $scope.InsertData = function () {
+        var Action = document.getElementById("btnSave").getAttribute("value");
+        if (Action == "Submit") {
+            $scope.Inventory= {};
+            $scope.Inventory.TeaName = $scope.itemName;
+            $scope.Inventory.Description = $scope.itemdescription;
+            $scope.Inventory.Price = $scope.itemprice;
+            $http({
+                method: "post",
+                url: "/Inventory/Insert_Inventory",
+                datatype: "json",
+                data: JSON.stringify($scope.Inventory)
+            }).then(function (response) {
+                console.log(response.data);
+                $scope.GetAllData();
+                $scope.itemName = "";
+                $scope.itemdescription = "";
+                $scope.itemprice="";
+            })
+        } else {
+            $scope.Inventory = {};
+            $scope.Inventory.TeaName = $scope.itemName;
+            $scope.Inventory.Description = $scope.itemdescription;
+            $scope.Inventory.Price = $scope.itemprice;
+            $scope.Inventory.Sr_no = document.getElementById("item_").value;
+            $http({
+                method: "post",
+                url: "/Inventory/Update_Inventory",
+                datatype: "json",
+                data: JSON.stringify($scope.Inventory)
+            }).then(function (response) {
+                console.log(response.data);
+                $scope.GetAllData();
+                $scope.itemName = "";
+                $scope.itemdescription = "";
+                $scope.itemprice = "";
+                document.getElementById("btnSave").setAttribute("value", "Submit");
+                document.getElementById("btnSave").style.backgroundColor = "cornflowerblue";
+                document.getElementById("spn").innerHTML = "Add New Item";
+            })
+        }
+    }
+    $scope.GetAllData = function () {
+        $http({
+            method: "get",
+            url: "/Inventory/Get_AllInventory"
+        }).then(function (response) {
+            $scope.TeaInventories = response.data;
+        }, function () {
+            alert("Error Occurred");
+        })
+    };
+    $scope.Deleteitem = function (item) {
+        $http({
+            method: "post",
+            url: "/Inventory/Delete_Inventory",
+            datatype: "json",
+            data: JSON.stringify(item)
+        }).then(function (response) {
+            console.log(response.data);
+            $scope.GetAllData();
+        })
+    };
+    $scope.Updateitem = function (item) {
+        document.getElementById("item_").value = item.Sr_no;
+        $scope.itemName = item.TeaName;
+        $scope.itemdescription = item.Description;
+        $scope.itemprice = item.Price;
+        document.getElementById("btnSave").setAttribute("value", "Update");
+        document.getElementById("btnSave").style.backgroundColor = "Black";
+        document.getElementById("spn").innerHTML = "Update Item Information";
+    };
+    //myApp.config(['$routeProvider',
+    //    function ($routeProvider) {
+    //        $routeProvider.
+    //            when("/Inventory/Index", {
+    //                templateUrl: "Inventory/Index"
+    //            })
+    //                .when('/Inventory/Get_InventoryById/:id', {
+    //                    templateUrl: "Inventory/Get_InventoryById",
+    //                    controller: "ItemsDetails"
+    //                })
+    //        otherwise({
+    //            redirectTo: '/Inventory/Index'
+    //        });
+    //    }])
+    //.controller("ItemsDetails", function ($scope, $http, $routeParams) {
+    //    $http({
+    //        url: "Inventory/Get_InventoryById",
+    //        params: { id: $routeParams.id },
+    //        method:"get"
+    //    }).
+    //    then(function (response) {
+    //        $scope.TeaInventory = response.data;
+    //    })
+
+    //});
+    //Redirect to Detail view from index view with parameter
+
+    //$scope.GetItemByID() = function (id) {
+
+    //    $http({
+    //        url: "Inventory/Get_InventoryById",
+    //        method: "get",
+    //        datatype: "json",
+    //        data: JSON.stringify($scope.Inventory)
+    //    }).then(function (response) {
+    //        $scope.TeaInventory = response.data;
+    //    }, function () {
+    //        alert("Error Occurred");
+    //    })
+    //};
+  
+    
+    //$scope.GetItemByID().success(function (id) {
+    //    document.getElementById("item_").value = item.Sr_no;
+    //    $scope.itemName = item.TeaName;
+    //    $scope.itemdescription = item.Description;
+    //    $scope.itemprice = item.Price;
+    //    $http({
+    //        method: "get",
+    //        url: "/Inventory/Get_AllInventory"
+    //    }).then(function (response) {
+    //        $scope.TeaInventories = response.data;
+    //    }, function () {
+    //        alert("Error Occurred");
+    //    })
+        
+    //});
+})
+
+
